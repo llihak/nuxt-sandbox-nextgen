3e2d49157d76130a0f5f5ec1626073a5be900614
keith hill <keith.hill@metastock.com>
2022-07-03 09:52:10 -0600

    generate

--- pages/powertools.vue
+++ pages/powertools.vue
@@ -3,8 +3,19 @@
     <div class="container mx-auto">
       <div class="powertools">
         <div class="powertool">
-          <div class="user-image">image</div>
-          <div class="blurb">blurb</div>
+          <div class="user-image">
+            <img
+              src="https://www.metastock.com/images/i/TRADER-EOD.png"
+              alt=""
+              class="sm:max-w-[150px]"
+            />
+          </div>
+          <div class="blurb">
+            <h3>Question: what the what?</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor a numquam aspernatur dolores tenetur
+              voluptas aut eaque impedit perspiciatis, distinctio ipsam reiciendis molestiae saepe laboriosam neque
+              fugiat exercitationem voluptates odio?</p>
+          </div>
         </div>
       </div>
     </div>
